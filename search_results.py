608c833b26257f479f8b0dfde3437ab95482afa5
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2020-12-17 18:35:11 -0700

    Fix bug in pruning.

--- search_results.py
+++ search_results.py
@@ -30,21 +30,21 @@ class SearchResults(object):
 
     def generate_png_file(self, out_file, root_person):
         person_subset = {root_person}
-        person_subset = person_subset.union({person for person in self.no_baptism_list})
-        person_subset = person_subset.union({person for person in self.no_endownment_list})
-        person_subset = person_subset.union({person for person in self.no_sealing_to_parents_list})
-        person_subset = person_subset.union({person for person in self.no_sealing_to_spouse_list})
+        person_subset = person_subset.union({relationship.person for relationship in self.no_baptism_list})
+        person_subset = person_subset.union({relationship.person for relationship in self.no_endownment_list})
+        person_subset = person_subset.union({relationship.person for relationship in self.no_sealing_to_parents_list})
+        person_subset = person_subset.union({relationship.person for relationship in self.no_sealing_to_spouse_list})
 
         print('Generating render tree...')
         visitation_set = set()
         root_node = root_person.generate_render_tree(visitation_set)
         size_before = root_node.calculate_size()
 
-        #print('Pruning render tree...')
-        #root_node.prune_tree(person_subset)
-        #size_after = root_node.calculate_size()
-        #percentage = 100.0 * size_after / size_before
-        #print('Tree reduced to %d%% of it\'s former size.' % percentage)
+        print('Pruning render tree...')
+        root_node.prune_tree(person_subset)
+        size_after = root_node.calculate_size()
+        percentage = 100.0 * size_after / size_before
+        print('Tree reduced to %d%% of it\'s former size.' % percentage)
 
         print('Calculating tree layout...')
         root_node.calculate_graph_layout()
